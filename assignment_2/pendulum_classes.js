2da3d95fa0b47a89a2c818e46734298d3a966148
cheesoup <55515711+cheesoup@users.noreply.github.com>
2019-10-31 06:32:02 -0400

    Add files via upload

--- assignment_2/pendulum_classes.js
+++ assignment_2/pendulum_classes.js
@@ -4,10 +4,11 @@
 var pause = false;
 var pendulum = new Array();
 
-// reverb to decrease audio resonance
-var reverb;
-var beep;
+// audio functions
+var reverb; // reverb cuz reverb is nice
+var beep; // synth for sound generation
 
+// note values to be played
 var note = new Array();
 var octave = new Array();
 
@@ -15,6 +16,7 @@ var octave = new Array();
 const gravity = 0.05;
 const stem = 300;
 
+// splash
 $(document).ready(function(){
   $("#start").click(function(){
       setup();
@@ -22,6 +24,7 @@ $(document).ready(function(){
   });
 });
 
+// re-randomize positions
 $('#reset').click(function() {
   for (var i = 0; i < pendulum.length; i++) {
     pendulum[i].mass = randInt(25, 25);
@@ -30,6 +33,7 @@ $('#reset').click(function() {
   }
 });
 
+// pause script
 $('#pause').click(function() {
   if (pause == false) {
     pause = true;
@@ -38,27 +42,29 @@ $('#pause').click(function() {
   }
 });
 
-//setup to pendulums
 function setup() {
-  $("#pendulums").css("visibility", "visible")
-  $("#start").css("visibility", "hidden")
-  reverb = new Tone.Freeverb ( {
+  $("#pendulums").css("visibility", "visible") // hide splash
+  $("#start").css("visibility", "hidden") // show pendulums
+
+  // initialize audio functions
+  reverb = new Tone.Freeverb ( { // https://tonejs.github.io/docs/13.8.25/Freeverb
     roomSize: 0.5,
     dampening: 500
   }).toMaster();
-  beep = new Tone.PolySynth(8, Tone.Synth, {
+  beep = new Tone.PolySynth(8, Tone.Synth, { // https://tonejs.github.io/docs/13.8.25/PolySynth
     oscillator : {
       type : "square"
     }
   }).connect(reverb);
 
+  // initialize pendulum
   pendulum[0] = new Pendulum(randInt(25, 25), posneg()*randInt(1.5, 0.05), stem, gravity, "pendulum0", "B5");
   pendulum[1] = new Pendulum(randInt(25, 25), posneg()*randInt(1.5, 0.05), stem, gravity, "pendulum1", "C6");
   pendulum[2] = new Pendulum(randInt(25, 25), posneg()*randInt(1.5, 0.05), stem, gravity, "pendulum2", "E6");
   pendulum[3] = new Pendulum(randInt(25, 25), posneg()*randInt(1.5, 0.05), stem, gravity, "pendulum3", "G6");
 }
 
-//function for retriggering class functions in order
+// function for retriggering class functions in order
 function loop() {
   if (pause == false) {
     for (var i = 0; i < pendulum.length; i++) {
@@ -71,7 +77,7 @@ function loop() {
   }
 }
 
-//randomizes negative and positive
+// randomizes negative and positive
 function posneg() {
   var randAr = new Array(-1, 1);
   return randAr[Math.round(Math.random())];
@@ -82,10 +88,10 @@ function randInt(i, o) {
   return Math.floor(Math.random()*i) + o;
 }
 
-//pendulum class!!!!!!!!!
+// pendulum class!!!!!!!!!
 class Pendulum {
   constructor(m, a, s, g, i, n) {
-    //initialize pendulum vars
+    // initialize pendulum vars
     this.origin = new Array(200,0);
     this.point = new Array(0,0);
     this.speed = 0;
@@ -93,7 +99,7 @@ class Pendulum {
     this.svg.setAttribute ("width", "400");
     this.svg.setAttribute ("height", "400");
 
-    //construct math vars
+    // construct math vars
     this.mass = m;
     this.angle = a;
     this.stem = s;
@@ -105,6 +111,8 @@ class Pendulum {
   }
 
   /*
+  Math based on: https://www.youtube.com/watch?v=9iaEqGOh5WM
+
   Math Notes:
   stem = 300
   stem = sqrt(x^2 + y^2)
@@ -131,28 +139,30 @@ class Pendulum {
   */
 
   update() {
-    //triangle/circle math
+    // triangle/circle math
     var inertia = Math.sin(this.angle) * (gravity * -1);
     var acceleration = inertia/this.mass;
     this.speed += acceleration;
     this.angle += this.speed;
 
-    //pendulum point update
+    // pendulum point update
     this.point[0] = Math.sin(this.angle) * this.stem + this.origin[0];
     this.point[1] = Math.cos(this.angle) * this.stem + this.origin[1];
 
-    //check if pendulum point has crossed the 0 degrees (pendulum in center position)
+    // check if pendulum point has crossed the 0 degrees (pendulum in center position)
     var lastpass = this.middlepass;
     if (this.angle > 0) this.middlepass = true;
     else if (this.angle <= 0) this.middlepass = false;
 
-    //play sound if so
+    // play sound if so
     if (lastpass != this.middlepass) {
       beep.triggerAttackRelease(this.note, "32n");
     }
   }
 
-  //draw pendulum to svg
+  // draw pendulum to svg
+  // code based on the following example:
+  // http://xahlee.info/js/js_scritping_svg_basics.html
   draw() {
     //clear last drawing if it exists
     while (this.svg.lastChild) {
@@ -160,21 +170,21 @@ class Pendulum {
     }
 
     //circle draw
-    const cir1 = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-    cir1.setAttribute("cx", this.point[0]);
-    cir1.setAttribute("cy", this.point[1]);
-    cir1.setAttribute("r", this.mass);
-    cir1.setAttribute("fill", "white");
-    this.svg.appendChild(cir1);
+    const cir = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+    cir.setAttribute("cx", this.point[0]);
+    cir.setAttribute("cy", this.point[1]);
+    cir.setAttribute("r", this.mass);
+    cir.setAttribute("fill", "white");
+    this.svg.appendChild(cir);
 
     //line draw
-    const line1 = document.createElementNS("http://www.w3.org/2000/svg", "line");
-    line1.setAttribute("x1", this.origin[0]);
-    line1.setAttribute("y2", this.origin[1]);
-    line1.setAttribute("x2", this.point[0]);
-    line1.setAttribute("y2", this.point[1]);
-    line1.setAttribute("style", "stroke:white; stroke-width:2px; stroke-linecap:round");
-    this.svg.appendChild(line1);
+    const line = document.createElementNS("http://www.w3.org/2000/svg", "line");
+    line.setAttribute("x1", this.origin[0]);
+    line.setAttribute("y2", this.origin[1]);
+    line.setAttribute("x2", this.point[0]);
+    line.setAttribute("y2", this.point[1]);
+    line.setAttribute("style", "stroke:white; stroke-width:2px; stroke-linecap:round");
+    this.svg.appendChild(line);
 
     //append drawing to svg
     document.getElementById(this.id).appendChild(this.svg);
